74a950ff72e17323d01f0d5b41fa8314a8291661
Tomer Morisi <tomer.morisi@gmail.com>
2021-05-17 08:08:10 -0400

    feat: add prettier and eslint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const express = require("express");
-require("./db/mongoose");
-const User = require("./models/user");
-const Task = require("./models/task");
+const express = require('express');
+require('./db/mongoose');
+const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
-app.post("/users", (req, res) => {
+app.post('/users', (req, res) => {
   console.log(req.body);
   const user = new User(req.body);
   user
@@ -15,7 +15,7 @@ app.post("/users", (req, res) => {
     .catch((err) => res.status(400).send(err));
 });
 
-app.post("/tasks", (req, res) => {
+app.post('/tasks', (req, res) => {
   console.log(req.body);
   const task = new Task(req.body);
   task
@@ -24,13 +24,13 @@ app.post("/tasks", (req, res) => {
     .catch((err) => res.status(400).send(err));
 });
 
-app.get("/users", (req, res) => {
+app.get('/users', (req, res) => {
   User.find({})
     .then((user) => res.send(user))
     .catch((err) => res.status(500).send());
 });
 
-app.get("/users/:id", (req, res) => {
+app.get('/users/:id', (req, res) => {
   const _id = req.params.id;
   User.findById(_id)
     .then((user) => {
@@ -41,5 +41,5 @@ app.get("/users/:id", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log("server is up on port ", port);
+  console.log('server is up on port ', port);
 });
